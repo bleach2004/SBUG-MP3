34384f194b8d0039578bfc632aa5c66f638fd3f3
bleach <nzm@s020.net>
2023-10-09 15:42:02 +0200

    change to relative path

--- SBUG_MP3/vite.config.js
+++ SBUG_MP3/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     build: {
     rollupOptions: {
       input: {
-        cutter: '/mp3-cutter/src/cutter.js',
-        AudioEditor: '/src/components/AudioEditor.jsx',
-        InputURL: '/src/components/InputURL.jsx',
-        app: '/src/app.jsx',
-        main: '/src/main.jsx',
+        cutter: 'mp3-cutter/src/cutter.js',
+        AudioEditor: 'src/components/AudioEditor.jsx',
+        InputURL: 'src/components/InputURL.jsx',
+        app: 'src/app.jsx',
+        main: 'src/main.jsx',
       },
     },
   },
