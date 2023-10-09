15dd28de82129779401ee83f260d37ceae1cd3f1
bleach <nzm@s020.net>
2023-10-09 15:25:13 +0200

    add mp3 cutter to build version

--- SBUG_MP3/vite.config.js
+++ SBUG_MP3/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.js',
+        cutter: 'mp3-cutter/src/cutter.js' // Make sure this path is correct
+      },
+    },
+  },
   
   plugins: [react()],
 })
